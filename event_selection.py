1cd80f024f466d01843a919d1b23d34434c4e67a
Jinkyu-So <jso@cern.ch>
2026-08-16 00:03:13 -0400

    Remove applycuts_dileptonic function
    Removed the applycuts_dileptonic function and its associated comments.

--- event_selection.py
+++ event_selection.py
@@ -1,4 +1,3 @@
-####################################################################################
 
 def applycuts_semileptonic(df, selection):
 
@@ -26,35 +25,3 @@ def applycuts_semileptonic(df, selection):
         df = df.Filter("tight_nbJet >= 2")
 
     return df
-
-####################################################################################
-
-def applycuts_dileptonic(df, selection):
-
-    step = int(selection.replace("s",""))
-    # default s0 : At least one good muon
-    # s1 : exactly two good muons with opposite sign
-    # s2 : MET cut
-    # s3 : jet multiplicity cut
-    # s4 : bjet requirement
-
-    df = df.Filter("nGoodMuon >= 2")
-
-    if step >= 1:
-        df = df.Filter("nGoodMuon == 2")
-        df = df.Filter("nExtraLooseMuon == 0")
-        df = df.Filter("nLooseElectron == 0")
-        df = df.Filter("GoodMuon_charge[0] * GoodMuon_charge[1] < 0")
-
-    if step >= 2:
-        df = df.Filter("nGoodJet >= 2")
-
-    if step >= 3:
-        df = df.Filter("medium_nbJet >= 2")
-
-    if step >= 4:
-        df = df.Filter("abs(ROOT::VecOps::InvariantMass(GoodMuon_pt, GoodMuon_eta, GoodMuon_phi, GoodMuon_mass) - 91.1876)> 15")
-
-    return df
-
-####################################################################################
