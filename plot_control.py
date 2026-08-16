eb0799aa981c181dd078584092ccda7c2d22be23
Jinkyu-So <jso@cern.ch>
2026-08-16 00:05:25 -0400

    Update plot_control.py

--- plot_control.py
+++ plot_control.py
@@ -1,8 +1,14 @@
 import ROOT
+
+
+############ need to replace TopRecoHelper ##############
 ROOT.gInterpreter.Declare('#include "TopRecoHelper.h"')
+
+
 from object_definition import define_objects
 from event_selection import applycuts_semileptonic, applycuts_dileptonic
-####################################################################################
+
+
 
 def get_objects():
     return{
@@ -284,15 +290,3 @@ samples_config_semileptonic = {
 }
 
 ####################################################################################
-
-samples_config_dileptonic = {
-    "Data":  ["run2018A_GLegacy_DiMuon_merged.root", "Data", ROOT.kBlack, True, 1987.090251629],
-    "TTbar": ["TTbar.root", "t#bar{t}", ROOT.kOrange-3, False, 100.0],
-    "QCD":   ["QCD.root", "QCD Multi-jet", ROOT.kAzure+7, False, 100.0],
-    "ST":    ["ST.root", "Single Top", ROOT.kRed-9, False, 100.0],
-    "DY":    ["DY.root", "Drell-Yan", ROOT.kSpring+5, False, 100.0],
-    "WJets": ["WJets.root", "W+Jets", ROOT.kGreen-3, False, 100.0],
-    "WW":    ["WW.root", "WW", ROOT.kYellow-7, False, 100.0],
-    "WZ":    ["WZ.root", "WZ", ROOT.kYellow-6, False, 100.0],
-    "ZZ":    ["ZZ.root", "ZZ", ROOT.kYellow-5, False, 100.0],
-}
